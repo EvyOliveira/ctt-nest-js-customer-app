4a60183cac33de5efa1827310a53c4121c47dbce
EvyOliveira <cristinioliveira.eo@gmail.com>
2021-03-12 12:38:57 -0300

    Refactor import CustomerController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomerController } from './customer.controller';
+import { CustomerController } from './controllers/customer.controller';
 import { CustomerService } from './controllers/customer.service';
 
 @Module({
